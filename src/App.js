cf9d6d6cc6ae03ad581c6ed6a9066282aa99a98f
Shahir Farhan <137508506+shahiirrrr@users.noreply.github.com>
2024-11-27 22:46:24 +0530

    PR: Enhance About component with image, description, CV download button, and Info component (#3)
    * Chore: Implement About component within the main layout structure
    
    * Feat: Add about image and CV PDF in assets for About component
    
    * Feat: Implement About component with image, description, and CV download button
    
    * Style: Update About component's CSS styles
    
    * Feat: Implement Info component with experience, completed projects, and support availability

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React from "react";
 import "./App.css";
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
+import About from "./components/about/About";
 
 /**
  * The App component is the main entry point for the React application.
- * It renders the Header component and the Home component within a main section.
  *
  * @component
  * @returns {JSX.Element} The rendered React application.
@@ -17,6 +17,7 @@ const App = () => {
 
       <main className="main">
         <Home />
+        <About />
       </main>
     </>
   );
